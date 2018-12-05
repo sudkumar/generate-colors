7f10875156435238b066971f6e6d7f09f1c20a8b
Sudhir Mitharwal <luckysud4@gmail.com>
2018-12-05 17:22:26 +0530

    update: added minification and runtime check for webpack and removed docs generation with publish command

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'docs/src/app.js'),
@@ -8,6 +9,15 @@ module.exports = {
     path: path.resolve(__dirname, 'docs')
   },
   mode: "production",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    runtimeChunk: true,
+    minimizer: [
+      new TerserPlugin()
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
